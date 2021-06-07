1cdff4b25897f99617f1c6e4b7ad5f8945a1af52
Maarifa Maarifa <76941589+MaarifaMaarifa@users.noreply.github.com>
2021-06-07 23:06:45 +0300

    Update MoonPhaseSimulator.py

--- MoonPhaseSimulator.py
+++ MoonPhaseSimulator.py
@@ -1,16 +1,16 @@
 from matplotlib import pyplot as plt
 from math import radians, sqrt, cos
 
-time_period = 27.32
-radius = 100                      # The graph moon radius
+time_period = 29.5
+radius = 100
 
 
 def turn_angle_finder(days_num):
     angle = (360 / time_period) * days_num
-    if angle > 360:
-        return angle % 360
-    else:
+    if angle < 360:
         return angle
+    else:
+        return angle % 360
 
 
 def dark_side_finder(angle):
@@ -21,29 +21,48 @@ def dark_side_finder(angle):
 
 
 def terminus_side_finder(angle):
-    if angle < 180:
-        if angle < 90:
-            return "right"
-        else:
-            return "left"
-    else:
-        if angle < 270:
-            return "right"
-        else:
-            return "left"
+    if 0 < angle <= 90:
+        return "right"
+    elif 90 < angle <= 180:
+        return "left"
+    elif 180 < angle <= 270:
+        return "right"
+    elif 270 < angle <= 360:
+        return "left"
+
+
+def phase_teller(angle):
+    if angle == 0 or angle == 360:
+        return "New Moon"
+    elif angle == 90:
+        return "First Quarter"
+    elif angle == 180:
+        return "Full Moon"
+    elif angle == 270:
+        return "Last Quarter"
+    elif 0 < angle < 90:
+        return "Waxing Crescent"
+    elif 90 < angle < 180:
+        return "Waxing Gibbous"
+    elif 180 < angle < 270:
+        return "Waning Gibbous"
+    elif 270 < angle < 360:
+        return "Waning Crescent"
+
 
 # A function for finding the x coordinates of the moon
-def circle_x_generator(x, r):
-    return sqrt(r**2 - x**2)
+def circle_x_generator(y, r):
+    return sqrt(r ** 2 - y ** 2)
+
 
 # A function for finding the x coordinates of the moon's terminus
 def ellipse_x_generator(y, r, angle):
     radian_angle = radians(angle)
-    return r * cos(radian_angle) * sqrt(1-(y/r)**2)
+    return abs(r * cos(radian_angle) * sqrt(1 - (y / r) ** 2))
 
 
 # Dealing with the moon(circle)
-y = list(range(-radius, (radius+1)))
+y = list(range(-radius, (radius + 1)))
 circle_x_positive = []
 circle_x_negative = []
 
@@ -71,6 +90,8 @@ elif terminus_side == "left":
     for num in y:
         ellipse_x.append(-ellipse_x_generator(num, radius, turn_angle))
 
+print(ellipse_x)
+print(terminus_side)
 plt.plot(ellipse_x, y, color="black")
 
 # Dealing with the moon shading(dark side)
@@ -79,6 +100,6 @@ if dark_side == "right":
 elif dark_side == "left":
     plt.fill_betweenx(y, circle_x_negative, ellipse_x, facecolor="grey")
 
-plt.title("The Moon")
+title = "Moon phase: " + phase_teller(turn_angle)
+plt.title(title)
 plt.show()
-
